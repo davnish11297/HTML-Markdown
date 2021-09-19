c73eda7f364bcd131cf7f4da9ebf0370dc4c8a2f
Davnish singh <davnishsingh46@gmail.com>
2021-09-19 05:14:51 -0700

    Added files

---
+++ Parser.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once 'html_markdown.php';
+
+/**
+ * This class parse the markdown text to HTML.
+ */
+class Parser extends HTMLMarkdown
+{
+
+    public function __construct() {
+    }
+
+    /**
+     * This method is used to parse the markdown text to HTML.
+     *
+     * @param string $Text
+     * @return string
+     */
+    public function ParseText($Text)
+    {
+        $HTMLMarkdown = new HTMLMarkdown($Text);
+        $Parsedtext = $HTMLMarkdown->Text();
+        
+        return $Parsedtext;
+    }
+}
